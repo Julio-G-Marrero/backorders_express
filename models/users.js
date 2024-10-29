7738fb9682a9e9d332ccf5bc6e334b022a7ce731
Julio <julio.garcia135@hotmail.com>
2024-10-29 11:50:10 -0600

    validate email

--- models/users.js
+++ models/users.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
 const userSchema = new mongoose.Schema({
   nombre: {
     type:String,
