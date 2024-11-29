cb493be82f30a0bea37b753e76274ceb7c9ceb85
Julio <julio.garcia135@hotmail.com>
2024-11-29 13:32:19 -0600

    mal request#

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,13 @@
-const routerProduct = require('express').Router();
-const multer = require("multer");
+// Ruta y controlador en Express para manejar la importación de productos
+const express = require('express');
+const multer = require('multer');
+
+const routerProduct = express.Router();
 const upload = multer({ dest: 'uploads/' });
-const { getProducts,createProduct,getProductsByValues,importCsvProducto } = require('../controllers/products');
 
-routerProduct.get('/',getProducts)
-routerProduct.get('/search/:all',getProductsByValues)
-routerProduct.post('/',createProduct)
-routerProduct.post("/import", upload.single("csv"),importCsvProducto);
+const { getProducts, createProduct, getProductsByValues, importCsvProducto } = require('../controllers/products');
 
-module.exports = routerProduct
+routerProduct.get('/', getProducts);
+routerProduct.get('/search/:all', getProductsByValues);
+routerProduct.post('/', createProduct);
+routerProduct.post('/import', upload.single('file'), importCsvProducto);
