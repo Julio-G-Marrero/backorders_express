ababba8acf51cff09ae930cc8fe101bdbb79893f
Julio <julio.garcia135@hotmail.com>
2024-11-29 13:12:30 -0600

    delete csv products

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,10 @@
 const routerProduct = require('express').Router();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-const { getProducts,createProduct,getProductsByValues,importProductosFromCSV } = require('../controllers/products');
+const { getProducts,createProduct,getProductsByValues } = require('../controllers/products');
 
 routerProduct.get('/',getProducts)
 routerProduct.get('/search/:all',getProductsByValues)
 routerProduct.post('/',createProduct)
-routerProduct.post("/import", upload.single("csv"), importProductosFromCSV);
 
 module.exports = routerProduct
