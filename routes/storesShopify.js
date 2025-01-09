af2fa66cc663a3591686a8572056b96caebfdc5b
Julio <julio.garcia135@hotmail.com>
2025-01-08 20:15:06 -0600

    node-conr

--- routes/storesShopify.js
+++ routes/storesShopify.js
@@ -3,6 +3,18 @@ const cron = require('node-cron');
 
 const { syncFirebirdWithShopify,saveLastSyncResults, getLastSyncResults, } = require('../controllers/storesShopify');
 
+// Sincronización inicial al iniciar el servidor
+(async () => {
+  console.log('Iniciando sincronización inicial...');
+  try {
+      const result = await syncFirebirdWithShopify();
+      saveLastSyncResults(result);
+      console.log('Sincronización inicial completada.');
+  } catch (error) {
+      console.error('Error en la sincronización inicial:', error.message);
+  }
+})();
+
 // Definimos la ruta para actualizar el inventario
 let isSyncInProgress = false;
 cron.schedule('0 */2 * * *', async () => {
@@ -11,6 +23,18 @@ cron.schedule('0 */2 * * *', async () => {
       const result = await syncFirebirdWithShopify();
       saveLastSyncResults(result);
       console.log('Sincronización completada.');
+  } catch (error) {
+      console.error('Error en la sincronización automática:', error.message);
+  }
+});
+
+// Programar sincronización automática cada 2 horas
+cron.schedule('0 */2 * * *', async () => {
+  console.log('Iniciando sincronización automática...');
+  try {
+      const result = await syncFirebirdWithShopify();
+      saveLastSyncResults(result);
+      console.log('Sincronización automática completada.');
   } catch (error) {
       console.error('Error en la sincronización automática:', error.message);
   }
