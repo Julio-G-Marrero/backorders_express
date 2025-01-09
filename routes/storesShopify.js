0d6b0cfb7cfd058479a2cd7090beb8f8d20061d8
Julio <julio.garcia135@hotmail.com>
2025-01-08 19:04:37 -0600

    NUEVA APIs

--- routes/storesShopify.js
+++ routes/storesShopify.js
@@ -1,22 +1,26 @@
 const routerShopify = require('express').Router();
-const { syncFirebirdWithShopify,fetchAllShopifyProducts } = require('../controllers/storesShopify');
+const { syncFirebirdWithShopify } = require('../controllers/storesShopify');
 
 // Definimos la ruta para actualizar el inventario
+let isSyncInProgress = false;
+
 routerShopify.get('/syncInventory', async (req, res) => {
+    if (isSyncInProgress) {
+        return res.status(400).json({ status: 'error', message: 'Sincronización en progreso, intente más tarde.' });
+    }
+
+    isSyncInProgress = true;
+    console.log(`Sincronización iniciada a las: ${new Date().toISOString()}`);
+
     try {
         const result = await syncFirebirdWithShopify();
-        res.json(result); // Devuelve el resumen en JSON
+        res.json(result);
     } catch (error) {
         res.status(500).json({ status: 'error', message: error.message });
+    } finally {
+        isSyncInProgress = false; // Liberar el bloqueo
+        console.log(`Sincronización finalizada a las: ${new Date().toISOString()}`);
     }
-});
-routerShopify.get('/products', async (req, res) => {
-  try {
-      const result = await fetchAllShopifyProducts();
-      res.json(result); // Devuelve el resumen en JSON
-  } catch (error) {
-      res.status(500).json({ status: 'error', message: error.message });
-  }
 });
 
 module.exports = routerShopify;
