d597c73b343254c80ab9e7b7c9c8e85bdcec667f
Julio <julio.garcia135@hotmail.com>
2025-01-08 18:43:56 -0600

    NUEVA APIs

--- routes/storesShopify.js
+++ routes/storesShopify.js
@@ -1,5 +1,5 @@
 const routerShopify = require('express').Router();
-const { syncFirebirdWithShopify } = require('../controllers/storesShopify');
+const { syncFirebirdWithShopify,fetchShopifyProducts } = require('../controllers/storesShopify');
 
 // Definimos la ruta para actualizar el inventario
 routerShopify.get('/syncInventory', async (req, res) => {
@@ -9,6 +9,14 @@ routerShopify.get('/syncInventory', async (req, res) => {
     } catch (error) {
         res.status(500).json({ status: 'error', message: error.message });
     }
+});
+routerShopify.get('/products', async (req, res) => {
+  try {
+      const result = await fetchShopifyProducts();
+      res.json(result); // Devuelve el resumen en JSON
+  } catch (error) {
+      res.status(500).json({ status: 'error', message: error.message });
+  }
 });
 
 module.exports = routerShopify;
